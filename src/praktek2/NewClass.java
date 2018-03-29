8c1f9ee76874750b4d891ebead2a58bd350538a6
AhmadSyarwani <32427962+AhmadSyarwani@users.noreply.github.com>
2018-03-29 15:54:37 +0800

    membuat behavior

--- src/praktek2/NewClass.java
+++ src/praktek2/NewClass.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek2;
 
-/**
- *
- * @author HARDWARE
- */
 public class NewClass {
+    Integer jumlahbarang;
+    Integer harga;
     
+    void cetakInfo (){
+        System.out.println("============================");
+        System.out.println("jumlahbarang" +jumlahbarang);
+        System.out.println("harga" +harga);
+        System.out.println("============================");
+    }
 }
